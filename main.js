a61a0510fc3c5a2675ae4297aee4adb08735accd
terrellturner <terrell@rellsoft.dev>
2024-01-01 22:14:50 -0600

    Responsive grid for skills and projects

---
+++ main.js
@@ -0,0 +1,15 @@
+import { library, icon } from "@fortawesome/fontawesome-svg-core";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+
+const techEls = document.getElementsByClassName("tech-skill-item");
+
+const techNames = [
+  "JavaScript",
+  "HTML+CSS",
+  "C#",
+  "React",
+  "Vue",
+  "Figma",
+  "InDesign",
+  "",
+];
